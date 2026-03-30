fba05dc01f02280fcc2210eba24ee1c3e46de036
Ashish6479 <aarya@solifetec.com>
2026-03-30 17:17:37 +0530

    aded two field

--- CaregiverLiteWebServices/CaregiverLite/Controllers/HomeController.cs
+++ CaregiverLiteWebServices/CaregiverLite/Controllers/HomeController.cs
@@ -24,11 +24,8 @@ namespace CaregiverLite.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-            string address555 = "delhi";
-            String City555 = "New York";
-
-
-            string age555 = "323223";
+            string name = "ashish";
+            string houseno = "1234";
             return View();
         }
     }
